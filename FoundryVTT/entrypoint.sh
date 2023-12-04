56cfd8be2f29021463931b48b94da2465452867c
rheuvel <rheuvel@sogyo.nl>
2023-12-04 21:25:16 +0100

    cleanup entrypoint

--- FoundryVTT/entrypoint.sh
+++ FoundryVTT/entrypoint.sh
@@ -7,21 +7,8 @@ else
 fi
 
 if ! test -f /app/foundryvtt/done; then
-    unzip /app/foundryvtt.zip -d /app/foundryvtt
-    touch /app/foundryvtt/done
+    unzip /app/foundryvtt.zip -d /app/foundryvtt \
+    && touch /app/foundryvtt/done
 else
     echo "FoundryVTT already unzipped"
 fi
-
-echo "###########"
-ls /
-echo "###########"
-ls /app/
-echo "###########"
-ls /app/foundryvtt/
-echo "###########"
-ls /app/foundryvtt/resources/
-echo "###########"
-ls /app/foundryvtt/resources/app/
-echo "###########"
-node /app/foundryvtt/resources/app/main.js --dataPath=/app/foundrydata
