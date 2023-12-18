b25531ad80c282659152eb388ec3183e253ba5ff
rheuvel <rheuvel@sogyo.nl>
2023-12-18 18:41:05 +0100

    fix nginx template replacements absolute

--- ReverseProxy/entrypoint.sh
+++ ReverseProxy/entrypoint.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 SUBS=$(echo $SUBLIST | tr ";" "\n")
 
-cp nginx-template-start.conf nginx-template.conf
+cp /etc/nginx/nginx-template-start.conf /etc/nginx/nginx-template.conf
 
 for SUB in $SUBS
 do
-    cat nginx-location-template.conf | sed 's|${LOCATION_SUB}|'${SUB}'|g' >> nginx-template.conf
+    cat /etc/nginx/nginx-location-template.conf | sed 's|${LOCATION_SUB}|'${SUB}'|g' >> /etc/nginx/nginx-template.conf
 done
 
-cat nginx-template-end.conf >> nginx-template.conf
+cat /etc/nginx/nginx-template-end.conf >> /etc/nginx/nginx-template.conf
 
 sed 's|${BASE_URL}|'${BASE_URL}'|g' /etc/nginx/nginx-template.conf > /etc/nginx/nginx.conf
 
