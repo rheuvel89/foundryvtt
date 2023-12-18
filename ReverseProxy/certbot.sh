ed226ee4e5e15aec31184e8fa4209ca9d930c74d
rheuvel <rheuvel@sogyo.nl>
2023-12-18 12:43:46 +0100

    certbot script eruit, zit nu in container

--- ReverseProxy/certbot.sh
+++
@@ -1,5 +0,0 @@
-# sudo certbot certonly --authenticator dns-azure --preferred-challenges dns --noninteractive --agree-tos --email r.heuvel89@gmail.com --dns-azure-config ~/.secrets/certbot/azure.ini -d vtt.t7l.org -d *.vtt.t7l.org
-sudo certbot renew
-sudo cp /etc/letsencrypt/live/sub.domain.com/fullchain.pem /usr/share/nginx/certificates/
-sudo cp /etc/letsencrypt/live/sub.domain.com/privkey.pem /usr/share/nginx/certificates/
-nginx -s reload
